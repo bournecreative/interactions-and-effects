713c3da1925ac37eb63d657518ffb9f8cde41c16
Christian <bournecreative@gmail.com>
2024-06-28 14:54:24 -0500

    create a parallax object that will init and add needed event listener if element exisits. ParallaxMotion method runs on scroll, translating the middle value of translate3d are different rate to illustrate movement at different rates

--- src/components/parallax/parallax.js
+++ src/components/parallax/parallax.js
@@ -1,10 +1,25 @@
 import './parallax.scss'
 
 export const parallax = {
+    
     initParallax() {
-        const parallaxContainer = document.querySelector('.parallax')
-        if (parallaxContainer) {
-            console.log('yes')
+        this.parallaxContainer = document.querySelector('.parallax')
+        if (this.parallaxContainer) {
+            window.addEventListener('scroll', this.parallaxMotion.bind(this))
         }
+    },
+    parallaxMotion() {
+        const first = this.parallaxContainer.querySelector('.parallax-text-first')
+        const mid = this.parallaxContainer.querySelector('.parallax-text-mid')
+        const last = this.parallaxContainer.querySelector('.parallax-text-last ')
+        const scrolled = window.scrollY
+        const rate1 = scrolled * 0.5
+        const rate2 = scrolled * 0.6
+        const rate3 = scrolled * 0.8
+        
+        first.style.transform = `translate3d(0px,${rate1}px,0px)`
+        mid.style.transform = `translate3d(0px,${rate2}px,0px)`
+        last.style.transform = `translate3d(0px,${rate3}px,0px)`
+        
     }
 }
