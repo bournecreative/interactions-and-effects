42e5bd9b51887e2b99529146a8061151c5fffaf0
Christian <bournecreative@gmail.com>
2024-06-28 14:51:11 -0500

    markup for our 3 elements

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ import { parallax } from './src/components/parallax/parallax'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <div class='parallax'></div> 
+    <div class='parallax'>
+      <span class='parallax-text'><span class='parallax-text-first'>par</span><span class='parallax-text-mid'>al</span><span class='parallax-text-last'>lax</span></span>
+    </div>
+    <div style="height: 100vh"></div>
+    <div style="height: 100vh"></div>
+    <div style="height: 100vh">END</div>
   </div>
 `
 
