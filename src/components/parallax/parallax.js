62ef1627ab27976c3e42caa187c1587efc5977c4
Christian <bournecreative@gmail.com>
2024-06-27 23:50:41 -0500

    setup for vanillia javascript components, very basic, just to prototype random UI rather then code pen

---
+++ src/components/parallax/parallax.js
@@ -0,0 +1,10 @@
+import './parallax.scss'
+
+export const parallax = {
+    initParallax() {
+        const parallaxContainer = document.querySelector('.parallax')
+        if (parallaxContainer) {
+            console.log('yes')
+        }
+    }
+}
